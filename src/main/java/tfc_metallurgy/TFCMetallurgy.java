77992881e90a7fa796601bdcb2784c0a2a7606d9
Pablo J Blasco <pablojblasco@gmail.com>
2026-06-19 12:52:08 +0200

    Fix anvils

--- src/main/java/tfc_metallurgy/TFCMetallurgy.java
+++ src/main/java/tfc_metallurgy/TFCMetallurgy.java
@@ -1,10 +1,13 @@
 package tfc_metallurgy;
 
 import com.mojang.logging.LogUtils;
+import net.dries007.tfc.common.blockentities.TFCBlockEntities;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
 import org.slf4j.Logger;
 import tfc_metallurgy.common.ClientEvents;
 import tfc_metallurgy.common.MetallumArmorMaterials;
@@ -13,6 +16,7 @@ import tfc_metallurgy.common.block_entities.MetallurgyBlockEntities;
 import tfc_metallurgy.common.blocks.MetallurgyBlocks;
 import tfc_metallurgy.common.fluids.MetallurgyFluids;
 import tfc_metallurgy.common.items.MetallurgyItems;
+import tfc_metallurgy.util.MetallurgyMetal;
 
 @Mod(TFCMetallurgy.mod_id)
 public class TFCMetallurgy {
@@ -30,8 +34,18 @@ public class TFCMetallurgy {
 		MetallurgyBlockEntities.BLOCK_ENTITIES.register(bus);
 		MetallurgyItemGroup.CREATIVE_TABS.register(bus);
 		bus.addListener(MetallurgyItemGroup::addCreative);
+		bus.addListener(TFCMetallurgy::addBlocksToBlockEntities);
 		if (FMLEnvironment.dist == Dist.CLIENT) {
 			ClientEvents.init(bus);
 		}
+	}
+
+	private static void addBlocksToBlockEntities(BlockEntityTypeAddBlocksEvent event)
+	{
+		Block[] anvilBlocks = MetallurgyBlocks.METALS.values().stream()
+			.filter(map -> map.containsKey(MetallurgyMetal.BlockType.ANVIL))
+			.map(map -> map.get(MetallurgyMetal.BlockType.ANVIL).get())
+			.toArray(Block[]::new);
+		event.modify(TFCBlockEntities.ANVIL.get(), anvilBlocks);
 	}
 }
--- src/main/java/tfc_metallurgy/common/block_entities/MetallurgyBlockEntities.java
+++ src/main/java/tfc_metallurgy/common/block_entities/MetallurgyBlockEntities.java
@@ -10,7 +10,6 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import tfc_metallurgy.TFCMetallurgy;
 import tfc_metallurgy.common.blocks.MetallurgyBlocks;
 
-import net.dries007.tfc.common.blockentities.AnvilBlockEntity;
 import tfc_metallurgy.util.MetallurgyMetal;
 
 import java.util.function.Supplier;
@@ -21,10 +20,6 @@ public class MetallurgyBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, TFCMetallurgy.mod_id);
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<MetallurgyBellBlockEntity>> BELL = register("bell", MetallurgyBellBlockEntity::new, Stream.of(MetallurgyBlocks.FLORENTINE_BRONZE_BELL, MetallurgyBlocks.BERYLLIUM_COPPER_BELL));
-
-    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<AnvilBlockEntity>> ANVIL = register("anvil",
-        (pos, state) -> new AnvilBlockEntity(MetallurgyBlockEntities.ANVIL.get(), pos, state, AnvilBlockEntity.AnvilInventory::new),
-        MetallurgyBlocks.METALS.values().stream().filter(map -> map.containsKey(MetallurgyMetal.BlockType.ANVIL)).map(map -> map.get(MetallurgyMetal.BlockType.ANVIL)));
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<MetallurgyLampBlockEntity>> LAMP = register("lamp", MetallurgyLampBlockEntity::new,
         MetallurgyBlocks.METALS.values().stream().filter(map -> map.containsKey(MetallurgyMetal.BlockType.LAMP)).map(map -> map.get(MetallurgyMetal.BlockType.LAMP)));
